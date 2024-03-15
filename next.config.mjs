4c70db214d7e925d50180cb82f37c6b3100dfb7c
Ivan <ivangpajon@hotmail.com>
2024-03-15 04:04:43 +0100

    chore: Change next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,11 @@
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
-  trailingSlash: true,
   basePath: '/github-actions-test',
-  assetPrefix: '/github-actions-test/', // assetPrefix requires the trailing slash
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
